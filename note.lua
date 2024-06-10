b79695b92370e3d26efe493d00b15eb8c7220b12
onncera-shogun <onncera@localhost.com>
2024-06-10 23:08:40 +0800

    system's (macOS) neovim

---
+++ note.lua
@@ -0,0 +1,29 @@
+
+--[[
+
+	Simple guide for Lua (https://learnxinyminutes.com/docs/lua/)
+	Simply use `:help lua-guide` as a reference for how Neovim integrates Lua
+
+	Run AND READ `:help`
+	Displays a help window with some basic information,
+		such as reading, navigating and searching the builtin help documentation
+
+	MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation
+
+--]]
+
+
+--[[
+	TODOS
+		Extensions to include
+			copilot.vim
+
+
+		Onncera's personal options
+			Find a better setup without sharing both neovim's & system's clipboard (primeagen remaps)
+			Sanction netrw settings once you are more comfortable using it
+
+		Onncera's personal remaps
+			Find a better remap for <esc>
+--]]
+
