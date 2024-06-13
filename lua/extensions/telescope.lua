8dac1eb6a9b0df8e52816221970235571f68882e
onncera-shogun <onncera@localhost.com>
2024-06-13 15:43:51 +0800

    improving neovim

--- lua/extensions/telescope.lua
+++ lua/extensions/telescope.lua
@@ -1,9 +1,9 @@
 return {
 
-	"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",  -- release branch
-		dependencies = { "nvim-lua/plenary.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },  -- run some command when plugin is installed or updated == `build'
+	'nvim-telescope/telescope.nvim',
+		branch = '0.1.x',  -- release branch
+		dependencies = { 'nvim-lua/plenary.nvim',
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },  -- only load when `make' is available
 		},
 
 		config = function()
