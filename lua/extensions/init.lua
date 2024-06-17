fa2a2dc66740886007c631f06557bdcb4e08b430
onncera-shogun <onncera@localhost.com>
2024-06-18 01:17:23 +0800

    included copilot & readme

---
+++ lua/extensions/init.lua
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+-- instructions for setting up copilot in neovim
+--		encompass GITHUB URL into plugin manager
+--		execute :Copilot setup
+--		login 2 NTU GITHUB & pass in OTP (FOUND IN MINIBUFFER)
+--
+--	COPILOT setup incomplete issues
+--		Suggestions matching public code (duplication detection filter)
+--		Set option "ALLOWED"
+
+
+
+
+
+
+return {
+
+
+
+
+
+
+	-- instructions for setting up copilot in neovim
+	{ "github/copilot.vim" },
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
